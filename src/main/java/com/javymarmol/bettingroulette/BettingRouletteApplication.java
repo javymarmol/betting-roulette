7e9f154fbd3e8a5d3f2056591905d41691edad76
javymarmol <contacto@javymarmol.com>
2021-01-19 00:04:36 -0500

    Initial commit

---
+++ src/main/java/com/javymarmol/bettingroulette/BettingRouletteApplication.java
@@ -0,0 +1,13 @@
+package com.javymarmol.bettingroulette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettingRouletteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BettingRouletteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javymarmol/bettingroulette/BettingRouletteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javymarmol.bettingroulette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BettingRouletteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
